497a6df39707f5842232603149f5fbf0e0acb744
Denis kipyegon <denis.kipyegon@equitybank.co.ke>
2025-02-13 15:41:37 +0300

    created the method to get the data from the API

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Users } from '../interfaces/users';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  private apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<Users[]> {
+    return this.http.get<Users[]>(this.apiUrl);
+  }
 }
